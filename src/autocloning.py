3a65a0a60e11cc3214d4cbc1f3d7279c3dfc8f9c
helberius <helberius@gmail.com>
2020-05-20 21:57:11 +0200

    test autocloning

---
+++ src/autocloning.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+import sys
+import subprocess
+import os
+import time
+
+if __name__ == "__main__":
+    print(str(sys.argv))
+    try:
+        bool_autocloning = bool(os.environ['AUTOCLONING'])
+        if bool_autocloning:
+            ls_parameters=['git','clone','https://github.com/helberius/maritime_tracking.git']
+            subprocess.cal(ls_parameters, shell=False)
+    except Exception as err:
+        print(repr(err))
